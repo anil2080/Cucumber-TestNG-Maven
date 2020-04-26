1c6abd4dd72843c1e611011c9bd37ce80fa8fe29
Anilkumar17 <anil.kumar2080@gmail.com>
2020-04-26 13:38:55 +0530

    First commit

---
+++ src/main/java/stepDefinitions/LoginStepDefinition.java
@@ -0,0 +1,58 @@
+package stepDefinitions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginStepDefinition {
+
+	WebDriver driver;
+
+	@Given("^user is already on Login Page$")
+	public void user_already_on_login_page() {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://ui.freecrm.com/");
+	}
+
+	@When("^title of login page is Free CRM$")
+	public void title_of_login_page_is_free_CRM() {
+		String title = driver.getTitle();
+		System.out.println(title);
+		Assert.assertEquals("Cogmento CRM", title);
+	}
+
+	@Then("^user enters \"(.*)\" and \"(.*)\"$")
+	public void user_enters_username_and_password(String username, String password) {
+		driver.findElement(By.name("email")).sendKeys(username);
+		driver.findElement(By.name("password")).sendKeys(password);
+	}
+
+	@Then("^user clicks on login button$")
+	public void user_clicks_on_login_button() {
+		WebElement loginBtn = driver.findElement(By.xpath("//*[@id=\"ui\"]/div/div/form/div/div[3]"));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();", loginBtn);
+	}
+
+	@Then("^user is on home page$")
+	public void user_is_on_hopme_page() {
+		String title = driver.getTitle();
+		System.out.println("Home Page title ::" + title);
+		Assert.assertEquals("Cogmento CRM", title);
+	}
+
+	@Then("^Close the browser$")
+	public void close_the_browser() {
+		driver.quit();
+	}
+
+}
---
+++ target/cucumber-reports/cucumber-pretty/report.js
@@ -0,0 +1,166 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Free CRM Login Feature",
+  "description": "",
+  "id": "free-crm-login-feature",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 3,
+  "name": "Free CRM Login Test Scenario",
+  "description": "",
+  "id": "free-crm-login-feature;free-crm-login-test-scenario",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "title of login page is Free CRM",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user enters \"\u003cusername\u003e\" and \"\u003cpassword\u003e\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 13,
+  "name": "",
+  "description": "",
+  "id": "free-crm-login-feature;free-crm-login-test-scenario;",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password"
+      ],
+      "line": 14,
+      "id": "free-crm-login-feature;free-crm-login-test-scenario;;1"
+    },
+    {
+      "cells": [
+        "anil.kumar2080@gmail.com",
+        "Test@1234"
+      ],
+      "line": 15,
+      "id": "free-crm-login-feature;free-crm-login-test-scenario;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 15,
+  "name": "Free CRM Login Test Scenario",
+  "description": "",
+  "id": "free-crm-login-feature;free-crm-login-test-scenario;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "title of login page is Free CRM",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user enters \"anil.kumar2080@gmail.com\" and \"Test@1234\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_already_on_login_page()"
+});
+formatter.result({
+  "duration": 14124730900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.title_of_login_page_is_free_CRM()"
+});
+formatter.result({
+  "duration": 169584200,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "anil.kumar2080@gmail.com",
+      "offset": 13
+    },
+    {
+      "val": "Test@1234",
+      "offset": 44
+    }
+  ],
+  "location": "LoginStepDefinition.user_enters_username_and_password(String,String)"
+});
+formatter.result({
+  "duration": 451391600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_clicks_on_login_button()"
+});
+formatter.result({
+  "duration": 65619500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_on_hopme_page()"
+});
+formatter.result({
+  "duration": 6908600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.close_the_browser()"
+});
+formatter.result({
+  "duration": 1514688700,
+  "status": "passed"
+});
+});
